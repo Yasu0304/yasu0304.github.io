a8b025666a3ed8ba11e3a5af28626c947b00576b
Yasu3051 <blackcat7134@gmail.com>
2022-01-04 12:04:05 +0800

    commit

--- jQuery/js/all.js
+++ jQuery/js/all.js
@@ -1,29 +1,30 @@
-$(document).ready(function () {
-    $(".dropdown").click(function (event) {
+$(document).ready(function() {
+    $(".navbar-dropdown").click(function(event) {
         event.preventDefault();
-        $(".dropdown-open").slideToggle();
-        $(".dropdown").toggleClass("active");
+        $(".navbar-dropdown-open").slideToggle();
+        $(".navbar-dropdown").toggleClass("active");
     });
     const swiper = new Swiper('.swiper', {
         // Optional parameters
         direction: 'horizontal',
         loop: true,
         effect: 'flip',
-      
+        allowSlideNext: true,
+
         // If we need pagination
         pagination: {
             el: '.swiper-pagination',
-          },
-      
+        },
+
         // Navigation arrows
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
-      
+
         // And if we need scrollbar
         scrollbar: {
-          el: '.swiper-scrollbar',
+            el: '.swiper-scrollbar',
         },
-      });
+    });
 });
