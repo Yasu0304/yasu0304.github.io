9a46b6dc07cfd371dd91c84e102b659699052762
Yasu3051 <blackcat7134@gmail.com>
2022-01-21 11:03:02 +0800

    master

--- jQuery/js/all.js
+++ jQuery/js/all.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
-    $(".navbar-dropdown").click(function(event) {
+    // $(".navbar-dropdown").click(function(event) {
+    //     event.preventDefault();
+    //     $(".navbar-dropdown-open").slideToggle();
+    //     $(".navbar-dropdown").toggleClass("active");
+    // });
+    $(".navbar-btn li").click(function(event) {
         event.preventDefault();
-        $(".navbar-dropdown-open").slideToggle();
-        $(".navbar-dropdown").toggleClass("active");
+        $(this).find(".navbar-dropdown-open").slideToggle();
+        $(this).find(".navbar-dropdown").toggleClass("active");
+        $(this).siblings().find("ul").slideUp();
+        $(this).siblings().find("a").removeClass("active");
     });
+    // 9. 下拉選單一次只會出現一個，其餘的進行收合。建議可修改成當點擊 "關於我們" 的時候，"產品介紹" 的選單會自動收合起來。
     const swiper = new Swiper('.swiper', {
         // Optional parameters
         direction: 'horizontal',
@@ -40,4 +48,13 @@ $(document).ready(function() {
             1000
         );
     });
+    function showBtnCondition() {
+        if ($(this).scrollTop() > 500) {
+          $('.top').fadeIn();
+            } else {
+             $('.top').fadeOut();
+           }
+         }
+     $(window).scroll(showBtnCondition);
+     // .top 在頁面最上方時可以改為不顯示，待頁面向下滾動一段距離後再顯示，
 });
