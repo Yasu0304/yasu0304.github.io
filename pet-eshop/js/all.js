3af08776fbf6c2e7127db2d8c4b401fbfb598e45
Yasu3051 <blackcat7134@gmail.com>
2022-04-01 12:47:41 +0800

    開始補細節

--- pet-eshop/js/all.js
+++ pet-eshop/js/all.js
@@ -5,4 +5,35 @@ $(document).ready(function () {
             prevEl: ".swiper-button-prev",
         },
     });
+
+    var swiper = new Swiper(".cartSwiper", {
+        slidesPerView: 2,
+        centeredSlides: false,
+        slidesPerGroupSkip: 3,
+        grabCursor: true,
+        keyboard: {
+            enabled: true,
+        },
+        breakpoints: {
+            769: {
+                slidesPerView: 5,
+                slidesPerGroup: 5,
+            },
+        },
+        // scrollbar: {
+        //     el: ".swiper-scrollbar",
+        // },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        // pagination: {
+        //     el: ".swiper-pagination",
+        //     clickable: true,
+        // },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+    });
 });
