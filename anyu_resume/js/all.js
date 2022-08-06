9d7728d0364b72d95977aab32b9b653c6249351c
Yasu3051 <blackcat7134@gmail.com>
2022-08-07 04:08:36 +0800

    整理照片

--- anyu_resume/js/all.js
+++ anyu_resume/js/all.js
@@ -13,35 +13,15 @@ $(document).ready(function () {
         },
             1000
         );
-    });
-    function showBtnCondition() {
-        if ($(this).scrollTop() > 1000) {
-            $('.top').fadeIn();
-        } else {
-            $('.top').fadeOut();
-        }
-    }
-    $(window).scroll(showBtnCondition);
-    // .top 在頁面最上方時可以改為不顯示，待頁面向下滾動一段距離後再顯示，
-    // $("body").on("click", "resume", function (event) {
-    //     //範圍裡.on("甚麼動作","哪個人")
-    //     event.preventDefault();
-    //     alert("有效喔!");
-    //     //執行事件
-    //   });
 
-    // $(".resume").click(function (event) {
-    //     event.preventDefault();
-    //     //取消a原本的預設動作
-    //     $("html, body").animate(
-    //         {
-    //             scrollTop: 0
-    //             //回到網頁的某點
-    //         },
-    //         1000
-    //         //速度控制
-    //     );
-    // });
+    });
+        // function showBtnCondition() {
+        //     if ($(this).scrollTop() > 1000) {
+        //         $('.top').fadeIn();
+        //     } else {
+        //         $('.top').fadeOut();
+        //     }
+        // }
 
     $(".open").click(function () {
         $(".none").slideDown();
